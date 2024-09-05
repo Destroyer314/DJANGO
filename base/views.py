63a4d7049c85de685ffd29f52f005058dbef77eb
Destroyer314 <matthewsherin17@gmail.com>
2024-09-04 20:08:43 -0400

    templates

--- base/views.py
+++ base/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, 'home.html')
 def room(request):
-    return HttpResponse("ROOM")
+    return render(request, 'room.html')

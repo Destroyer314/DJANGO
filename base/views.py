3b59aeab6b04ac575e28b6dd3e52fb990eecccb5
Destroyer314 <matthewsherin17@gmail.com>
2024-09-15 14:44:10 -0400

    Activity Feed & User Profile Page

--- base/views.py
+++ base/views.py
@@ -75,7 +75,8 @@ def home(request):
     
     topic = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topic , 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    context = {'rooms': rooms, 'topics': topic , 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -92,6 +93,14 @@ def room(request, pk):
         return redirect('room', pk=room.id)
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html',context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html',context)
 
 @login_required(login_url='login')
 def createRoom(request):
